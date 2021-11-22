9e3e809a891a527b4a36d62c64058ed4684f4b33
kd <khanhdungl@gmail.com>
2021-11-22 13:24:02 -0800

    fininshed sections about weather questions

--- scripts/bot.js
+++ scripts/bot.js
@@ -4,33 +4,82 @@
 
 const axios = require ("axios")
 
-
 module.exports = function (bot) {
-    “Good morning, Matt! There’s no storm in the forecast, but you’ll bring the thunder wearing your T-shirt and shorts.”
     bot.hear(/Good Morning!/, function(res){
-        return res.send('Stop talking to me please!')
+        return res.send('Good morning! What can I help you with today?')
     })
 
     bot.hear(/What day is it today?/i, function(res){
         return res.send(new Date().toDateString())
-
     })
 
     bot.respond(/What do you want to do today?/i, function(res){
         return res.send("I'm too fat to do anything!")
     })
 
-    bot.respond(/What's the weather in (.*)/i, async function(msg){
-        let name
-        name = msg.match[1]
-        console.log(name)
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=19c4a780bd0f2058ff8b95dd621da84e&units=imperial`
+//Questions about yelp
+    bot.respond(/What's the temp in (.*)/i, async function(msg){
+        let cityName
+        cityName = msg.match[1]
+        console.log(cityName)
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=19c4a780bd0f2058ff8b95dd621da84e&units=imperial`
+
+        let response = await axios.get(url)    
+    }) 
+
+//Questions about weather
+    bot.respond(/What's the temp in (.*)/i, async function(msg){
+        let cityName
+        cityName = msg.match[1]
+        console.log(cityName)
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=19c4a780bd0f2058ff8b95dd621da84e&units=imperial`
 
         let response = await axios.get(url)
-        console.log(response.data)
+        if (response.data.main.temp < 50){
+            return msg.send(`It's currently ` + response.data.main.temp + ' fahrenheit in ' + cityName + '. You should probably wear a jacket!')    
+        } else if (response.data.main.temp > 70){
+            return msg.send(`It's currently ` + response.data.main.temp + ' fahrenheit in ' + cityName + '. Dress lightly. Its going to be hot!')
+        } else { 
+            return msg.send(`It's currently ` + response.data.main.temp + ' fahrenheit in ' + cityName)
+        }       
+    }) 
 
-        
+    bot.respond(/What's the weather like in (.*)/i, async function(msg){
+        let cityName
+        cityName = msg.match[1]
+        console.log(cityName)
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=19c4a780bd0f2058ff8b95dd621da84e&units=imperial`
+
+        let response = await axios.get(url)
+        return msg.send(response.data.weather[0].description)
     })
+
+        bot.respond(/when is sunrise in (.*)/i, async function(msg){
+        let cityName
+        cityName = msg.match[1]
+        console.log(cityName)
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=19c4a780bd0f2058ff8b95dd621da84e&units=imperial`
+
+        let response = await axios.get(url)
+        let seconds = response.data.sys.sunrise
+        let date = new Date(seconds * 1000)
+        let timeString = date.toLocaleTimeString()
+        return msg.send('Sunrise in ' + cityName + ' is at ' + timeString)
+    }) 
+
+    bot.respond(/when is sunset in (.*)/i, async function(msg){
+        let cityName
+       
+        cityName = msg.match[1]
+        console.log(cityName)
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=19c4a780bd0f2058ff8b95dd621da84e&units=imperial`
+
+        let response = await axios.get(url)
+        let seconds = response.data.sys.sunset
+        let date = new Date(seconds *1000)
+        let timestr = date.toLocaleTimeString() 
+        return msg.send('Sunset in ' + cityName + ' is at ' + timestr)
+    }) 
 
 }
 
